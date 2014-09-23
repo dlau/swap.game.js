fa5a69bb190f5498eaedd0854f5701af4c3ba975
Daryl Lau <daryl@weak.io>
2014-09-22 23:43:15 -0700

    avalanche, fading

--- front/js/game.jsx
+++ front/js/game.jsx
@@ -3,6 +3,8 @@ var React = require('react');
 var _ = require('lodash');
 
 var GRID_ELEMENT_WIDTH = 30;
+var GRID_WIDTH = 10;
+var GRID_HEIGHT = 15;
 var DEFAULT_ANIMATION_DURATION = 500;
 
 //helpers
@@ -17,8 +19,8 @@ var neighborsWithColor = function(grid, x, y, color){
   var neighborsFound = [];
   for(var i=0;i < offsets.length; ++i){
     var offset = [offsets[i][0] + x, offsets[i][1] + y];
-    if(offset[0] >= 0 && offset[0] < 10 &&
-       offset[1] >=0 && offset[1] < 10){
+    if(offset[0] >= 0 && offset[0] < GRID_WIDTH &&
+       offset[1] >=0 && offset[1] < GRID_HEIGHT){
       var neighborColor = grid[offset[0]][offset[1]].color;
       if(neighborColor && neighborColor === color){
         neighborsFound.push(offset);
@@ -49,25 +51,55 @@ var cleanFrom = function(grid, x, y, color, moved){
   return grid;
 };
 
+var avalanche = function(grid, cb){
+  //process column-wise, squash and move all the white space to the top
+  var inner = function(){
+    var i,j, gapFound, freeCell;
+    for(i = 0;i < GRID_WIDTH; ++i){
+      colorFound = false;
+      j = freeCell = GRID_HEIGHT - 1;
+      for(; j >=0; --j){
+        if(grid[i][j].color !== null){
+          grid[i][freeCell--].color = grid[i][j].color;
+        }
+      }
+      for(j = freeCell; j >= 0; --j){
+        if(grid[i][j].color){
+          gapFound = true;
+        }
+        grid[i][j].color = null;
+      }
+    }
+    setTimeout(function(){
+      //optimize later
+      for(i = 0; i < GRID_WIDTH; ++i){
+        for(j = 0; j < GRID_HEIGHT; ++j){
+          if(grid[i][j].color !== null){
+            grid = cleanFrom(grid, i, j, grid[i][j].color);
+          }
+        }
+      }
+      if(gapFound){
+        return setTimeout(inner, 500);
+      }
+      else{
+        cb(grid);
+      }
+    }, 500);
+  };
+  inner();
+};
+
 
 var Grid = React.createClass({
   getInitialState : function(){
     //this should work dynamically
     return {
-      width : 10,
-      height : 10,
-      grid : _.times(10, function(){
-        return _.times(10, function(){
-          return {
-            color : null
-          };
-        });
-      }),
       secondsElapsed : 0
     };
   },
   componentWillMount : function(){
-    var grid = this.state.grid;
+    var grid = this.props.grid;
     _.each(grid, function(e, i){
       _.each(e, function(e, j){
         var colors = _.shuffle(['#2ecc71','#8e44ad', '#e74c3c']);
@@ -84,7 +116,7 @@ var Grid = React.createClass({
   selectGridElement : function(x, y, e){
     //this is really bad, find a better way to do this
     var rect = e.target.parentElement.parentElement.getBoundingClientRect();
-    var grid = this.state.grid;
+    var grid = this.props.grid;
     var offsetX = (e.pageX - rect.left) / GRID_ELEMENT_WIDTH - x;
     var offsetY = (e.pageY - rect.top) / GRID_ELEMENT_WIDTH - y;
     var offsets = [
@@ -98,7 +130,7 @@ var Grid = React.createClass({
       var dst = e[1];
       if(offsetX > cond.xMin && offsetX < cond.xMax &&
          offsetY > cond.yMin && offsetY < cond.yMax &&
-         dst[0] < 10 && dst[0] >= 0 && dst[1] < 10 && dst[1] >=0){
+         dst[0] < GRID_WIDTH && dst[0] >= 0 && dst[1] < GRID_HEIGHT && dst[1] >=0){
         if(grid[x][y].color && grid[dst[0]][dst[1]].color){
           this.animateSwap([x, y], dst, DEFAULT_ANIMATION_DURATION);
         }
@@ -108,7 +140,7 @@ var Grid = React.createClass({
   },
   animateSwap : function(src, dst, duration){
     var FRAMERATE = 1000/30; //25fps
-    var grid = this.state.grid;
+    var grid = this.props.grid;
     if(!grid[src[0]][src[1]].animateL){
       grid[src[0]][src[1]].animateL = grid[src[0]][src[1]].animateR = 0;
       grid[dst[0]][dst[1]].animateL = grid[dst[0]][dst[1]].animateR = 0;
@@ -126,9 +158,7 @@ var Grid = React.createClass({
       grid[dst[0]][dst[1]].animateR = diffY;
 
       //movement speed, distance/time
-      this.setState({
-        grid : grid
-      });
+      this.props.setGrid(grid);
       duration -= FRAMERATE * (DEFAULT_ANIMATION_DURATION / 1000);
       setTimeout(this.animateSwap.bind(null, src, dst, duration), FRAMERATE);
     }
@@ -145,25 +175,26 @@ var Grid = React.createClass({
       cleanFrom(grid, src[0], src[1], grid[src[0]][src[1]].color);
       cleanFrom(grid, dst[0], dst[1], grid[dst[0]][dst[1]].color);
       this.props.onMove();
-      return this.setState({
-        grid : grid
-      });
+      return this.props.setGrid(grid);
     }
   },
   render : function(){
-    var gridElements = _.map(this.state.grid, function(e, i){
+    var gridElements = _.map(this.props.grid, function(e, i){
       return <div className="grid-row">
         {_.map(e, function(e, j){
           var style = {
-            background : e.color,
             'margin-left' : GRID_ELEMENT_WIDTH * i,
             'margin-top' : GRID_ELEMENT_WIDTH * j
           };
+          if(e.color){
+            style.background = e.color;
+          }
           style['margin-left'] = e.animateL ? style['margin-left'] + e.animateL : style['margin-left'];
           style['margin-top'] = e.animateR ? style['margin-top'] + e.animateR : style['margin-top'];
+          var squareClassName = 'grid-square';
           return (
             <div
-              className={"grid-square" +(e.color ? " bevel" : "")} 
+              className={"grid-square" +(e.color ? " bevel" : " blank")} 
               style={style} key={"grid-" + i + "-" + j}
               onClick={_.bind(this.selectGridElement, null, i, j)}
             >
@@ -173,8 +204,8 @@ var Grid = React.createClass({
         </div>
     }.bind(this));
     var gridStyle = {
-      width : GRID_ELEMENT_WIDTH * 10,
-      height : GRID_ELEMENT_WIDTH * 10
+      width : GRID_ELEMENT_WIDTH * GRID_WIDTH,
+      height : GRID_ELEMENT_WIDTH * GRID_HEIGHT
     };
     return (
       <div className="game-grid" style={gridStyle}>
@@ -216,7 +247,7 @@ var Controls = React.createClass({
   render : function(){
     return (
       <div className="game-controls">
-        <button>
+        <button onClick={this.props.onAvalanche}>
           Avalanche
         </button>
         <button>
@@ -233,9 +264,21 @@ var Controls = React.createClass({
 var Game = React.createClass({
   getInitialState : function(){
     return {
+      grid : _.times(GRID_WIDTH, function(){
+        return _.times(GRID_HEIGHT, function(){
+          return {
+            color : null
+          };
+        });
+      }),
       nMoves : 0,
       secondsElapsed : 0
     };
+  },
+  setGrid : function(grid){
+    this.setState({
+      grid : grid
+    });
   },
   componentDidMount : function(){
     setInterval(function(){
@@ -249,12 +292,17 @@ var Game = React.createClass({
     var onMove = function(e){
       this.setState({ nMoves : this.state.nMoves + 1 });
     }.bind(this);
+    var onAvalanche = function(){
+      avalanche(this.state.grid, function(grid){
+        this.setGrid(grid);
+      }.bind(this));
+    }.bind(this);
     return (
       <div>
-        <Grid onMove={onMove}/>
+        <Grid grid={this.state.grid} onMove={onMove} setGrid={this.setGrid}/>
         <div className="game-right">
           <ScoreBoard nMoves={this.state.nMoves} secondsElapsed={this.state.secondsElapsed} />
-          <Controls />
+          <Controls onAvalanche={onAvalanche} />
         </div>
       </div>
     );
